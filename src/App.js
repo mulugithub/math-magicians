c299c85b8eb6cea4bb1513eb66915265fe970cd2
Muluken Zemed <zmmulu@gmail.com>
2023-08-30 22:14:27 +0300

    Apply Routes and Route component to Todo APP

--- src/App.js
+++ src/App.js
@@ -1,14 +1,22 @@
 import './App.css';
+import { Routes, Route } from 'react-router-dom';
+import NotMatch from './components/NotMatch';
+import Navbar from './components/Navbar';
+import Home from './components/Home';
 import Calculator from './components/Calculator';
 import Quotes from './components/Quotes';
 
 const App = () => (
   <div className="App">
-    <header className="App-header">
-      <p> Math Magicians App</p>
-    </header>
+    <Navbar />
     <Calculator />
     <Quotes />
+    <Routes>
+      <Route path="*" element={<NotMatch />} />
+      <Route path="/" element={<Home />} />
+      <Route path="/Calculator" element={<Calculator />} />
+      <Route path="/Quotes" element={<Quotes />} />
+    </Routes>
   </div>
 );
 
